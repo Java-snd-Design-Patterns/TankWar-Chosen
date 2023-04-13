a64684d86bc44d08f0187578368d73a7463db134
10994 <mmengcx@gmail.com>
2023-04-14 07:58:22 +0800

    test github00

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test the github");
+        System.out.println("1");
     }
 }
