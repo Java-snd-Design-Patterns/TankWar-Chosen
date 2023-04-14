6fb832f9355f654db32f6145c654bbe67f3ff477
Chosen-MengChuxuan121 <93848282+Chosen-MengChuxuan121@users.noreply.github.com>
2023-04-14 08:00:39 +0800

    Delete Test.java

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-import org.w3c.dom.ls.LSOutput;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Test the github");
-        System.out.println("1");
-    }
-}
